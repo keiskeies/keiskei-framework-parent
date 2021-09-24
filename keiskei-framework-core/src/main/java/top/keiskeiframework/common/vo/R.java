867e618aaca0946003690c0748a2b992b44e78e5
陈加敏 <chenjiamin@oschina.cn>
2021-09-24 16:15:59 +0800

    优化

--- keiskei-framework-core/src/main/java/top/keiskeiframework/common/vo/R.java
+++ keiskei-framework-core/src/main/java/top/keiskeiframework/common/vo/R.java
@@ -1,6 +1,9 @@
 package top.keiskeiframework.common.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.Getter;
 import top.keiskeiframework.common.enums.exception.ApiErrorCode;
 import top.keiskeiframework.common.enums.exception.IErrorCode;
 
@@ -14,20 +17,20 @@ import java.io.Serializable;
  * </p>
  * @since 2020/11/22 21:19
  */
-@Data
+@ApiModel(value = "R", description = "统一返回结构")
 public class R<T> implements Serializable {
     private static final long serialVersionUID = 4464081726731277091L;
-    /**
-     * 状态码
-     */
+
+    @ApiModelProperty(value = "状态码", dataType = "long", notes = "0: 正常")
+    @Getter
     private long code;
-    /**
-     * 数据
-     */
+
+    @ApiModelProperty(value = "数据", dataType = "T")
+    @Getter
     private T data;
-    /**
-     * 信息
-     */
+
+    @ApiModelProperty(value = "错误信息", dataType = "String", notes = "正常")
+    @Getter
     private String msg;
 
 
@@ -53,9 +56,9 @@ public class R<T> implements Serializable {
 
     private static <T> R<T> restResult(T data, long code, String msg) {
         R<T> apiResult = new R<>();
-        apiResult.setCode(code);
-        apiResult.setData(data);
-        apiResult.setMsg(msg);
+        apiResult.code = code;
+        apiResult.data = data;
+        apiResult.msg = msg;
         return apiResult;
     }
 
